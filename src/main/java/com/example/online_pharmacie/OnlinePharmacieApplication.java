b489a6ae0074731d517365824915b868e82445f5
HaitamP <haytam.modli2004@gmail.com>
2025-11-15 19:17:47 +0100

    first commit

---
+++ src/main/java/com/example/online_pharmacie/OnlinePharmacieApplication.java
@@ -0,0 +1,13 @@
+package com.example.online_pharmacie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinePharmacieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlinePharmacieApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/online_pharmacie/OnlinePharmacieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.online_pharmacie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinePharmacieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
